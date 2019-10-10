c455d307047671c3ac4b725201ef3afbd4f0c3fb
Jazmin M <jazminmerckel@gmail.com>
2019-10-10 01:18:35 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+ 
+  def name=(persons_name)
+    @name
+  end
+ 
+  def name
+    @name
+  end
+ 
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end
+ 
+  def breed
+    @breed
+  end
+end

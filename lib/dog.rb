b156267dbf11bbd61413eb65382e7928d0df94c4
Jazmin M <jazminmerckel@gmail.com>
2019-10-10 01:21:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,25 +1,6 @@
 class Dog
  
   def name=(dogs_name)
-    @name = dogs_name
-  end
- 
-  def name
-    @name
-  end
- 
-  def breed=(persons_job)
-    @job = persons_job
-  end
- 
-  def job
-    @job
-  end
-end
-
-class Dog
- 
-  def name=(dogs_name)
     @name
   end
  

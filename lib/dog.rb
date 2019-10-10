b0e3e15fc4a90fccd5ef1c9418e7d4fa06f7a53e
Jazmin M <jazminmerckel@gmail.com>
2019-10-10 01:16:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,8 +10,21 @@ class Dog
   def bark
     puts "woof!"
   end
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
   
-  
+  def bark
+    puts "woof!"
+  end
+end
+ 
+
   
   
 end
